857009cbb768a94abdeaa673a0b786ca520808af
Martin <martin.nygard@live.no>
2024-10-13 11:02:19 +0200

    icon added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Theme from "./styles/theme";
 import App from "./App";
